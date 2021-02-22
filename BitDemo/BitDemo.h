705ad0280b1387e6d82fbedb30f9cf7f9c69cd38
Andrew <andrey-garanin-00@mail.ru>
2021-02-22 15:39:07 +0300

    Добавьте файлы проекта.

---
+++ BitDemo/BitDemo.h
@@ -0,0 +1,33 @@
+﻿
+// BitDemo.h: основной файл заголовка для приложения BitDemo
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"       // основные символы
+
+
+// CBitDemoApp:
+// Сведения о реализации этого класса: BitDemo.cpp
+//
+
+class CBitDemoApp : public CWinApp
+{
+public:
+	CBitDemoApp() noexcept;
+
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+	UINT  m_nAppLook;
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBitDemoApp theApp;
